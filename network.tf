b36c28f010c8ba8ebeb6c47051e21f524c481a3c
Mike Dabydeen <Mdabydeen@conestogac.on.ca>
2025-06-04 18:40:32 -0400

    Reorg Files and added initial load balancer
    Moved files into logical structure.
    
    Added the initial config for a load balancer
    
    Author: Mike Dabydeen <mdabydeen@conestogac.on.ca>

---
+++ network.tf
@@ -0,0 +1,39 @@
+# NETWORKING
+resource "aws_vpc" "app" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+}
+
+# Internet GW 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.app.id
+
+  tags = {
+    Name = "IGW-CondorMatics-S25"
+  }
+}
+
+# Subnet 
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.app.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Route Table 
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.app.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+# Route Table Association 
+resource "aws_route_table_association" "public_subnet_route_table" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
