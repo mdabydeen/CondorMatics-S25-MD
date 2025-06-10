1352b3d1d91d8ae4c85d14441169e0203ad0af20
Mike Dabydeen <mdabydeen@conestogac.on.ca>
2025-06-10 19:13:30 -0400

    W6: Add Terraform configuration file.
    Terraform block is used to build terraform configuration and define its
    providers.
    
    Providers are sourced from the Terraform registry
    https://registry.terraform.io/providers
    
    For now, we will be using the Terraform and AWS providers
    
    Author: Mike Dabydeen <mdabydeen@conestogac.on.ca>

---
+++ terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">=1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+
+  backend "s3" {
+    bucket = "condormatics-s25-tfstate-md"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
