b9a8b7ed1700a85849f04e4963abe76b9c9af917
Mike Dabydeen <mdabydeen@conestogac.on.ca>
2025-06-18 19:07:11 -0400

    W7: Add database and configuration.
    Introduced adding a RDS database to our configuration.
    
    Added the parameter group, subnet group and security group configuration
    to allow for connecting to the database.
    
    Some work still needs to be done for future configuration.
    
    Author: Mike Dabydeen <mdabydeen@conestogac.on.ca>

--- security-groups.tf
+++ security-groups.tf
@@ -37,7 +37,28 @@ resource "aws_security_group" "load_balancer_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
   tags = {
     Name = "lb-security-group"
   }
+}
+
+resource "aws_security_group" "pg_security_group" {
+  name = "rds_pg"
+  vpc_id =  aws_vpc.app.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
