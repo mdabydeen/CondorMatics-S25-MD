917dcccc6684afbebf4e4ce0a895010be3a3ba76
Mike Dabydeen <mdabydeen@conestogac.on.ca>
2025-07-08 18:59:47 -0400

    W10: Added new db config + CI/CD
    - Added the initial github actions.
    - Changed DB to use Aurora instead.
    
    Author: Mike Dabydeen <mdabydeen@conestogac.on.ca>

--- database.tf
+++ database.tf
@@ -1,14 +1,17 @@
-resource "aws_db_instance" "postgres_db" {
-  identifier             = "local-postgres-db"
-  engine                 = "postgres"
-  engine_version         = "16.6"
-  instance_class         = "t2.large"
-  allocated_storage      = 20
-  username               = "username"
-  password               = "password"
-  db_subnet_group_name   = aws_db_subnet_group.pg_subnet_group.name
-  parameter_group_name   = aws_db_parameter_group.pg_parameter_group.name
+resource "aws_rds_cluster" "pg_db_cluster" {
+  engine         = "aurora-postgresql"
+  engine_version = "16.6"
+  master_username      = "username"
+  master_password      = "password"
+  db_subnet_group_name = aws_db_subnet_group.pg_subnet_group.name
   vpc_security_group_ids = [aws_security_group.pg_security_group.id]
-  publicly_accessible    = true
   skip_final_snapshot    = true
+}
+
+resource "aws_rds_cluster_instance" "aurora_pg_instance" {
+  cluster_identifier  = aws_rds_cluster.pg_db_cluster.id
+  engine              = aws_rds_cluster.pg_db_cluster.engine
+  engine_version      = aws_rds_cluster.pg_db_cluster.engine_version
+  instance_class      = "db.t3.medium"
+  publicly_accessible = true
 }
